d26844093ed30991296fd27191d9357c38355859
user4git4python <alma60.arreguin@gmail.com>
2024-04-07 21:06:07 -0600

    update VOLUME_PATH /app/images/uploads/

--- ads/settings.py
+++ ads/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
- 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
