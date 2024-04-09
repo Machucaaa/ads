e928d53d236af3f47882774a6fd0dbc5cb32d001
user4git4python <alma60.arreguin@gmail.com>
2024-04-09 10:57:23 -0600

    static files in prod 2nd intento

--- ads/settings.py
+++ ads/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -143,6 +140,7 @@ STATICFILES_DIRS = [
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-643f.up.railway.app']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
