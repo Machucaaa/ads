d947d5fab66903a1d6ecdc03907085406f2405f8
user4git4python <alma60.arreguin@gmail.com>
2024-04-09 22:15:57 -0600

    static files 7to intento

--- ads/settings.py
+++ ads/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'ads.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
