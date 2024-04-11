212da925be2725692680ad2585a017d25fc6cc27
user4git4python <alma60.arreguin@gmail.com>
2024-04-11 00:02:17 -0600

    2otar vez debug=True

--- ads/settings.py
+++ ads/settings.py
@@ -141,7 +141,9 @@ STATIC_URL = '/static/'
 
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STORAGES = {
-    # ...
+   "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
