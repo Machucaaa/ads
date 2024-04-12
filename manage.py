a4518ecad1a4b08feb16eec73f0ce6f627e627ff
user4git4python <alma60.arreguin@gmail.com>
2024-04-12 12:06:36 -0600

    4 settings original

--- manage.py
+++ manage.py
@@ -3,14 +3,10 @@
 import os
 import sys
 
-from ads.settings import base
 
 def main():
     """Run administrative tasks."""
-    if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ads.settings')
-    else: 
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ads.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ads.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
