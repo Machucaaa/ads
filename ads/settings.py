e97cf32e09b19b7fbaba1ba05d27a44da4c981bb
user4git4python <alma60.arreguin@gmail.com>
2024-04-12 15:34:17 -0600

    production atempt 2

--- ads/settings.py
+++ ads/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG=os.environ.get('DEBUG')
+DEBUG=False
 ALLOWED_HOSTS = ['mxbajio.up.railway.app','127.0.0.1']
 
 
