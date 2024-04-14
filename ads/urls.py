3aced67009c71d3d35a4b4830ae9f72d93aba767
user4git4python <alma60.arreguin@gmail.com>
2024-04-13 21:25:31 -0600

    production atempt 19

--- ads/urls.py
+++ ads/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('404/', handling_404 ),       
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
